bbce7600864c5ce3fdd7fefd39153d6dbb88c5e6
ulaskorpe <ulaskorpe@gmail.com>
2017-06-13 14:59:51 +0300

    topla up

--- topla.php
+++ topla.php
@@ -8,11 +8,17 @@ public function topla($x,$y){
 	return $x+$y;
 }
 
+public function cikar($x,$y){
+	return $x-$y;
+}
+
 }
 
 
 $islemler = new Islemler;
 
 echo $islemler->topla(404,56);
+
+echo $islemler->cikar(404,56);
 
 ?>
