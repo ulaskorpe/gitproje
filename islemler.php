c8c64edd1732391e28aa2a4f3c88a26ae46269d7
ulaskorpe <ulaskorpe@gmail.com>
2017-06-13 15:33:00 +0300

    islemler

---
+++ islemler.php
@@ -0,0 +1,24 @@
+<?php
+
+
+class Islemler {
+
+
+public function topla($x,$y){
+	return $x+$y;
+}
+
+public function cikar($x,$y){
+	return $x-$y;
+}
+
+}
+
+
+$islemler = new Islemler;
+
+echo $islemler->topla(404,56);
+
+echo $islemler->cikar(404,56);
+
+?>
