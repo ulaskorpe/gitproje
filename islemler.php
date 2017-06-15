62fc04df53245edc9e0fcebbe3a06c468dee2657
ulaskorpe <ulaskorpe@gmail.com>
2017-06-15 11:11:45 +0300

    islemler up

--- islemler.php
+++ islemler.php
@@ -16,13 +16,18 @@ public function kalan($x,$y){
 	return $x%$y;
 }
 
+public function rastgele($x=0,$y=1000){
+	return rand($x,$y);
+}
+
 }
 
 
 $islemler = new Islemler;
 
 echo $islemler->topla(404,56);
-
+echo "<hr>";
 echo $islemler->cikar(404,56);
-
+echo "<hr>";
+echo $islemler->rastgele(10,11);
 ?>
