15f20bed57334b0a69889d8692705d24f4d57d65
ulaskorpe <ulaskorpe@gmail.com>
2017-06-15 11:56:52 +0300

    rename

--- anasayfa.php
+++ anasayfa.php
@@ -1,26 +1,6 @@
 <?php
 
-
-class Islemler {
-
-
-public function topla($x,$y){
-	return $x+$y;
-}
-
-public function cikar($x,$y){
-	return $x-$y;
-}
-
-public function kalan($x,$y){
-	return $x%$y;
-}
-
-public function rastgele($x=0,$y=1000){
-	return rand($x,$y);
-}
-
-}
+include "class.php";
 
 
 $islemler = new Islemler;
